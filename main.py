380e87982b3a900e251f5bf9793bcc200ffccb22
skip090 <93792897+skip090@users.noreply.github.com>
2021-11-07 21:10:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ Hi {}, I'm Forward Tag Remover bot.\n\nForward me some messages, i will remove f
 
 START_BTN = InlineKeyboardMarkup(
         [[
-        InlineKeyboardButton('Source Code', url='https://github.com/samadii/ChannelForwardTagRemover'),
+        InlineKeyboardButton('👉 Owner 👈', url='https://t.me/Loltage'),
         ]]
     )
 
